a063fc4c2fca5f5b019830d4db3b8be1bd31feb3
grandeto <grandeto.com@gmail.com>
2019-07-21 15:08:12 +0300

    Add has_many reference and dependent action to RssSource Model

--- app/models/rss_source.rb
+++ app/models/rss_source.rb
@@ -1,4 +1,5 @@
 class RssSource < ApplicationRecord
+  has_many :rss_articles, dependent: :destroy
   validates :name, presence: true, format: { with: /[a-zA-Z0-9_]/ }, length: { in: 3..255 }
   validates :url, presence: true, url: true, uniqueness: true
 end
