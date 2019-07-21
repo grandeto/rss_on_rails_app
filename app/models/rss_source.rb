96d3770e35b1a10af7f27ab98d39e74975c93949
grandeto <grandeto.com@gmail.com>
2019-07-21 14:50:36 +0300

    Add validations to RssSource Model

--- app/models/rss_source.rb
+++ app/models/rss_source.rb
@@ -1,2 +1,4 @@
 class RssSource < ApplicationRecord
+  validates :name, presence: true, format: { with: /^[a-zA-Z0-9_]*$/ }, length: { in: 3..255 }
+  validates :url, presence: true, url: true, uniqueness: true
 end
