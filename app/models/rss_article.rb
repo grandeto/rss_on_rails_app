81a0211748a9efeec974e65ee90a367a2342b23d
grandeto <grandeto.com@gmail.com>
2019-07-21 16:52:58 +0300

    Add RssArticle model validations

--- app/models/rss_article.rb
+++ app/models/rss_article.rb
@@ -1,3 +1,7 @@
 class RssArticle < ApplicationRecord
   belongs_to :rss_source
+  validates :rss_source_id, presence: true, numericality: true
+  validates :ext_article_id, presence: true, format: { with: /[a-zA-Z0-9_]/ }
+  validates :title, presence: true, format: { with: /[a-zA-Z0-9_]/ }, length: { maximum: 255 }
+  validates :url, presence: true, url: true, uniqueness: true
 end
