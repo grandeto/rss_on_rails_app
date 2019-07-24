e90311ed5a5c4e5678135684a7da7538afac6ce7
grandeto <grandeto.com@gmail.com>
2019-07-24 04:59:44 +0300

    Add Home page view and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'home#index'
+  get 'home/index'
   resources :rss_articles, only: %i[index destroy]
   resources :rss_sources
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
